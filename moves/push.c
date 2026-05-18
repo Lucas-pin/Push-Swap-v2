4ae11b370a895080ec2c023e8deda8a75c5e9ca2
Lucas-pin <lupin@c2r10s5.42malaga.com>
2026-05-18 21:36:30 +0200

    [FEAT] 'push' movements

---
+++ moves/push.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lupin <lupin@student.42malaga.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/18 21:11:11 by lupin             #+#    #+#             */
+/*   Updated: 2026/05/18 21:36:02 by lupin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+static t_stack	*isolate(t_stack **lst)
+{
+	t_stack	*aux;
+
+	if (!lst || !*lst)
+		return (NULL);
+	aux = *lst;
+	if (aux->prev)
+		aux->prev->next = aux->next;
+	if (aux->next)
+		aux->next->prev = aux->prev;
+	aux->prev = NULL;
+	aux->next = NULL;
+	return (aux);
+}
+
+void	push(t_stack **dst, t_stack **src)
+{
+	if (!dst || !src)
+		return ;
+	add_node_front(dst, isolate(src));
+}
+
+void	pb(t_stack *stack_a, t_stack *stack_b)
+{
+	t_stack	*first_a;
+	t_stack	*first_b;
+
+	if (!stack_a || !stack_b)
+		return ;
+	first_a = first_node(stack_a);
+	first_b = first_node(stack_b);
+	push(&stack_a, &stack_b);
+}
+
+void	pa(t_stack *stack_a, t_stack *stack_b)
+{
+	t_stack	*first_a;
+	t_stack	*first_b;
+
+	if (!stack_a || !stack_b)
+		return ;
+	first_a = first_node(stack_a);
+	first_b = first_node(stack_b);
+	push(&stack_b, &stack_a);
+}
