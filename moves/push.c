298515272bbc1e61d1659dc8f73012e1394c42ad
Jaime Ruiz Agullo <jruiz-ag@c1r6s1.42malaga.com>
2026-05-20 19:08:40 +0200

    [FIX] Fixing push to actualise both pointers

--- moves/push.c
+++ moves/push.c
@@ -3,27 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lupin <lupin@student.42malaga.com>         +#+  +:+       +#+        */
+/*   By: jruiz-ag <jruiz-ag@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 21:11:11 by lupin             #+#    #+#             */
-/*   Updated: 2026/05/18 21:39:24 by lupin            ###   ########.fr       */
+/*   Updated: 2026/05/20 19:07:23 by jruiz-ag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-static t_stack	*isolate(t_stack **lst)
+static t_stack	*push_isolate(t_stack **lst)
 {
 	t_stack	*aux;
 
 	if (!lst || !*lst)
 		return (NULL);
 	aux = *lst;
-	if (aux->prev)
-		aux->prev->next = aux->next;
-	if (aux->next)
-		aux->next->prev = aux->prev;
-	aux->prev = NULL;
+	*lst = aux->next;
+	if (*lst)
+		(*lst)->prev = NULL;
 	aux->next = NULL;
 	return (aux);
 }
@@ -32,29 +30,19 @@ static void	push(t_stack **dst, t_stack **src)
 {
 	if (!dst || !src)
 		return ;
-	add_node_front(dst, isolate(src));
+	add_node_front(dst, push_isolate(src));
 }
 
-void	pb(t_stack *stack_a, t_stack *stack_b)
+void	pa(t_stack **stack_a, t_stack **stack_b)
 {
-	t_stack	*first_a;
-	t_stack	*first_b;
-
 	if (!stack_b)
 		return ;
-	first_a = first_node(stack_a);
-	first_b = first_node(stack_b);
-	push(&stack_a, &stack_b);
+	push(stack_a, stack_b);
 }
 
-void	pa(t_stack *stack_a, t_stack *stack_b)
+void	pb(t_stack **stack_a, t_stack **stack_b)
 {
-	t_stack	*first_a;
-	t_stack	*first_b;
-
 	if (!stack_a)
 		return ;
-	first_a = first_node(stack_a);
-	first_b = first_node(stack_b);
-	push(&stack_b, &stack_a);
+	push(stack_b, stack_a);
 }
