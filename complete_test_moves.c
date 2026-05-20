fe18f8dd4d92b85db7dc16ca3cba159f42510453
Jaime Ruiz Agullo <jruiz-ag@c1r6s1.42malaga.com>
2026-05-20 20:58:48 +0200

    [TEST] Proves all of the movements to evaluate it

---
+++ complete_test_moves.c
@@ -0,0 +1,60 @@
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+	int	i;
+	t_stack *list_a;
+	t_stack *head_a;
+	t_stack *list_b;
+	t_stack *head_b;
+
+	i = 0;
+	++argv;
+	ft_printf("=================== INITIAL TEST ===================\n\n");
+	if (argc <= 1)
+	{
+		ft_printf("[TEST] No arguments\n\n");
+		ft_printf("=================== END TEST ===================\n\n");
+		return (0);
+	}
+	ft_printf("[TEST] Testing program with argv: \n");
+	while (argv[i])
+		ft_printf("[TEST]-> %s\n", argv[i++]);
+	list_a = first_node((const t_stack *)build_list(argv));
+	list_b = NULL;
+	pb(&list_a, &list_b);
+	ra(&list_a);
+	pb(&list_a, &list_b);
+	pb(&list_a, &list_b);
+	ra(&list_a);
+	ra(&list_a);
+	pb(&list_a, &list_b);
+	pb(&list_a, &list_b);
+	pa(&list_a, &list_b);
+	ra(&list_a);
+	pa(&list_a, &list_b);
+	ra(&list_a);
+	pa(&list_a, &list_b);
+	ra(&list_a);
+	pa(&list_a, &list_b);
+	rra(&list_a);
+	pa(&list_a, &list_b);
+	rra(&list_a);
+	head_a = list_a;
+	while (list_a)
+	{
+		ft_printf("[TEST A] node content: %i\n", list_a->value);
+		list_a = list_a->next;
+	}
+	head_b = list_b;
+	while (list_b)
+	{
+		ft_printf("[TEST B] node content: %i\n", list_b->value);
+		list_b = list_b->next;
+	}
+	free_stack(&head_a);
+	free_stack(&head_b);
+
+	ft_printf("=================== END TEST ===================\n\n");
+	return (0);
+}
