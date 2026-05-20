86f3ef23ca1ff17b5bc6d2ad8f78e2ddc88216b1
Jaime Ruiz Agullo <jruiz-ag@c1r6s1.42malaga.com>
2026-05-20 19:43:58 +0200

    [FEAT] Swap (sa, sb, ss) implemented with the whole functionality

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jruiz-ag <jruiz-ag@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 21:05:28 by lupin             #+#    #+#             */
-/*   Updated: 2026/05/20 19:06:45 by jruiz-ag         ###   ########.fr       */
+/*   Updated: 2026/05/20 19:36:22 by jruiz-ag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,14 @@ void	free_stack(t_stack **lst);
 t_stack	*build_list(char **argv);
 
 /**
+ * @brief Take the first element from stack b and place it at the top of stack a.
+ * @note It does nothing if b is empty.
+ * @param stack_a Pointer to any node of stack a.
+ * @param stack_b Pointer to any node of stack b.
+ */
+void	pa(t_stack **stack_a, t_stack **stack_b);
+
+/**
  * @brief Take the first element from stack a and place it at the top of stack b.
  * @note It does nothing if a is empty.
  * @param stack_a Pointer to any node of stack a.
@@ -95,11 +103,25 @@ t_stack	*build_list(char **argv);
 void	pb(t_stack **stack_a, t_stack **stack_b);
 
 /**
- * @brief Take the first element from stack b and place it at the top of stack a.
- * @note It does nothing if b is empty.
+ * @brief Take the first two elements from stack a and swap them.
+ * @note It does nothing if a is empty or only has one element.
  * @param stack_a Pointer to any node of stack a.
+ */
+void	sa(t_stack **stack_a);
+
+/**
+ * @brief Take the first two elements from stack b and swap them.
+ * @note It does nothing if b is empty or only has one element.
  * @param stack_b Pointer to any node of stack b.
  */
-void	pa(t_stack **stack_a, t_stack **stack_b);
+void	sb(t_stack **stack_b);
+
+/**
+ * @brief Take the first two elements from stack a and stack b and swap them.
+ * @note It swap any stack if has at least two elements.
+ * @param stack_a Pointer to any node of stack a. 
+ * @param stack_b Pointer to any node of stack b.
+ */
+void	ss(t_stack **stack_a, t_stack **stack_b);
 
 #endif
