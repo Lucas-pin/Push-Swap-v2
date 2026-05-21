92072b397b363b77c458423d3a605a644e4ca6ed
Jaime Ruiz Agullo <jruiz-ag@c1r2s1.42malaga.com>
2026-05-21 14:54:43 +0200

    [FIX] complete_test_moves.c proving insertion_sort

--- complete_test_moves.c
+++ complete_test_moves.c
@@ -10,6 +10,7 @@ int main(int argc, char **argv)
 
 	i = 0;
 	++argv;
+	
 	ft_printf("=================== INITIAL TEST ===================\n\n");
 	if (argc <= 1)
 	{
@@ -17,26 +18,32 @@ int main(int argc, char **argv)
 		ft_printf("=================== END TEST ===================\n\n");
 		return (0);
 	}
-	ft_printf("[TEST] Testing program with argv: \n");
+	/*
+	//ft_printf("[TEST] Testing program with argv: \n");
 	while (argv[i])
 		ft_printf("[TEST]-> %s\n", argv[i++]);
+	*/
 	list_a = first_node((const t_stack *)build_list(argv));
 	if (!list_a)
 		return (-1);
 	list_b = NULL;
 	insertion_sort(&list_a, &list_b);
 	head_a = list_a;
+	/*
 	while (list_a)
 	{
 		ft_printf("[TEST A] node content: %i\n", list_a->value);
 		list_a = list_a->next;
 	}
+	*/
 	head_b = list_b;
+	/*
 	while (list_b)
 	{
 		ft_printf("[TEST B] node content: %i\n", list_b->value);
 		list_b = list_b->next;
 	}
+	*/
 	free_stack(&head_a);
 	free_stack(&head_b);
 
