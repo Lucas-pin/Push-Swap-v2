d3b5cec5fe476bc5df966e5c54b735a02a850b27
Lucas-pin <lupin@c1r12s2.42malaga.com>
2026-05-21 22:49:42 +0200

    [REFACTOR] clarify the insertion sort algorithm

--- algorithms/insertion_sort.c
+++ algorithms/insertion_sort.c
@@ -3,32 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   insertion_sort.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jruiz-ag <jruiz-ag@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: lupin <lupin@student.42malaga.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 22:19:28 by lupin             #+#    #+#             */
-/*   Updated: 2026/05/21 14:47:48 by jruiz-ag         ###   ########.fr       */
+/*   Updated: 2026/05/21 22:38:50 by lupin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-// Find the maximum number in stack which is lower than value
-static int	find_lower_pos(t_stack *stack, int value)
+/**
+ * @brief Find the position into stack where value 
+ * should be inserted to keep the order
+ * @param stack The stack to search
+ * @param value The value to insert
+ * @param order The order to keep (ASC or DESC)
+ * @return The position where value should be inserted,
+ * or -1 if it can't be inserted
+ */
+static int	find_pos(t_stack *stack, int value, t_order order)
 {
 	int	pos;
 	int	index;
-	int	prev_value;
+	int	prev;
 
 	index = 0;
-	prev_value = -2147483648;
+	prev = INT_MIN;
 	if (!stack)
-		return (-1);
-	pos = -1;
+		return (ERROR);
+	pos = ERROR;
 	while (stack)
 	{
-		if (value > stack->value && prev_value <= stack->value)
+		if ((value > stack->value && prev <= stack->value && order == DESC)
+			|| (value < stack->value && prev <= stack->value && order == ASC))
 		{
-			prev_value = stack->value;
+			prev = stack->value;
 			pos = index;
 		}
 		index++;
@@ -37,36 +46,16 @@ static int	find_lower_pos(t_stack *stack, int value)
 	return (pos);
 }
 
-// Find the maximum number in stack which is higher than value
-static int	find_higher_pos(t_stack *stack, int value)
-{
-	int	pos;
-	int	index;
-	int	prev_value;
-
-	index = 0;
-	prev_value = -2147483648;
-	if (!stack)
-		return (-1);
-	pos = -1;
-	while (stack)
-	{
-		if (value < stack->value && prev_value <= stack->value)
-		{
-			prev_value = stack->value;
-			pos = index;
-		}
-		index++;
-		stack = stack->next;
-	}
-	return (pos);
-}
-
+/**
+ * @brief Find the maximum value in the stack
+ * @param stack The stack to search
+ * @return The maximum value in the stack
+ */
 static int	max_value(t_stack *stack)
 {
 	int	max;
 
-	max = -2147483648;
+	max = INT_MIN;
 	while (stack)
 	{
 		if (stack->value > max)
@@ -76,40 +65,51 @@ static int	max_value(t_stack *stack)
 	return (max);
 }
 
-void	rotate_stack(t_stack **stack, int pos)
+/**
+ * @brief Rotate the stack to bring the element 
+ * at the specified position to the top
+ * @param stack The stack to rotate
+ * @param pos The position of the element to bring to the top
+ */
+static void	rotate_stack(t_stack **stack, int pos)
 {
 	int	size;
+	int	count;
 
 	size = lst_size(*stack);
 	if (pos <= size / 2)
 	{
-		while (pos-- > 0)
+		count = pos;
+		while (count-- > 0)
 			rb(stack);
 	}
 	else
 	{
-		pos = size - pos;
-		while (pos-- > 0)
+		count = size - pos;
+		while (count-- > 0)
 			rrb(stack);
 	}
 }
 
-void	insertion_sort(t_stack **stack_a, t_stack **stack_b)
+int	insertion_sort(t_stack **stack_a, t_stack **stack_b)
 {
 	int	pos;
+	int	higher_pos;
 
 	pb(stack_a, stack_b);
 	while (*stack_a)
 	{
-		pos = find_lower_pos(*stack_b, (*stack_a)->value);
-		if (pos == -1)
-			pos = find_higher_pos(*stack_b, (*stack_a)->value);
-		if (pos == -1)
-			return ;
+		pos = find_pos(*stack_b, (*stack_a)->value, DESC);
+		if (pos == ERROR)
+			pos = find_pos(*stack_b, (*stack_a)->value, ASC);
+		if (pos == ERROR)
+			return (ERROR);
 		rotate_stack(stack_b, pos);
 		pb(stack_a, stack_b);
 	}
-	rotate_stack(stack_b, find_lower_pos(*stack_b, max_value(*stack_b) + 1));
+	higher_pos = find_pos(*stack_b, max_value(*stack_b) + 1, DESC);
+	rotate_stack(stack_b, higher_pos);
 	while (*stack_b)
 		pa(stack_a, stack_b);
+	return (0);
 }
